b32d847548e340038a7b7d516eb14229357ea9ed
Lindsay Cox <lindsaygcox@live.co.uk>
2020-11-22 11:51:12 +0000

    Retrieved some code from shooter project

---
+++ ListExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CoxlinCore
+{
+    public static class ListExtensions
+    {
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+
+        public static List<T> GetChunk<T>(this List<T> source, int startIndex, int endIndex)
+        {
+            var list = new List<T>();
+            for (int i = startIndex; i < endIndex; ++i)
+            {
+                list.Add(source[i]);
+            }
+            return list;
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            var rand = new System.Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ RandomExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoxlinCore
+{
+    public static class RandomExtensions
+    {
+        public static T GetRandom<T>(this IList<T> list)
+        {
+            return GetRandom<T>(list.ToArray());
+        }
+
+        public static T GetRandom<T>(this T[] array)
+        {
+            if (array.Length <= 0)
+            {
+                return default(T);
+            }
+
+            Random rand = new Random();
+            return array[rand.Next(0, array.Length - 1)];
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable)
+        {
+            Random rand = new Random();
+            int next = rand.Next();
+            return enumerable.OrderBy(x => next);
+        }
+
+
+    }
+}
