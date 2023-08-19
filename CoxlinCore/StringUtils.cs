dbf36f7f46dea8fc1d893f61460784532e354bb8
Lindsay <lindsaygcox@live.co.uk>
2023-08-19 19:18:05 +0100

    Found StringUtils in Getting There and added some new GS code to it

---
+++ CoxlinCore/StringUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CoxlinCore
+{
+    public static class StringUtils
+    {
+        public static string UppercaseFirst(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+
+        public static string LowercaseFirst(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            char[] a = s.ToCharArray();
+            a[0] = char.ToLowerInvariant(a[0]);
+            return new string(a);
+        }
+
+        public static bool Contains(this string s, string check)
+        {
+            return s.IndexOf(check, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+    }
+}
